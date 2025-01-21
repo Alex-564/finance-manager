698ff576d5bff5024d9beaba9a79572b76440f58
Alex-564 <alex.murray5037@gmail.com>
2025-01-21 20:18:01 +0000

    added gradle skeleton, added basic file reader functionality

---
+++ app/src/main/java/utils/file/CSVParser.java
@@ -0,0 +1,52 @@
+package utils.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CSVParser implements FileReader {
+    
+    @Override
+    public ArrayList<String> readFile(String fileName){
+        ArrayList<String> fileData = new ArrayList<String>();
+
+        //attempt to open passed file
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+
+            //collect each line
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+
+                if (data.isBlank()) break;
+                fileData.add(data);
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Does Not Exist!");
+        }
+
+        return fileData;
+    }
+
+    @Override
+    public void getFileInfo(String fileName){
+        File myObj = new File(fileName);
+
+        if (myObj.exists()){
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable " + myObj.canRead());
+            System.out.println("File size in bytes " + myObj.length());
+        } else {
+            System.out.println("File Does Not Exist!");
+
+        }
+    }
+
+}
+
---
+++ app/src/main/java/utils/file/FileReader.java
@@ -0,0 +1,11 @@
+package utils.file;
+
+//imports
+import java.util.ArrayList;
+
+public interface FileReader {
+
+    public ArrayList<String> readFile(String fileName);
+    public void getFileInfo(String fileName);
+    
+}
