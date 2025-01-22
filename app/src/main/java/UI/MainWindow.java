167f7e3e953884d1721ed8a8e53bbb451e929e25
Alex-564 <alex.murray5037@gmail.com>
2025-01-22 17:55:23 +0000

    added a basic menu bar to main window

--- app/src/main/java/UI/MainWindow.java
+++ app/src/main/java/UI/MainWindow.java
@@ -1,38 +1,70 @@
 package UI;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class MainWindow {
+public class MainWindow extends JFrame implements ActionListener{
     
+    //private JFrame mainWindow;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenu helpMenu;
+    JMenuItem exportItem;
+    JMenuItem exitItem;
 
     public MainWindow(){
     
-        JFrame mainWindow = new JFrame();
+        //mainWindow = new JFrame();
 
         //Default settings for main application window
-        mainWindow.setTitle("Finance Manager");
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainWindow.setResizable(false);
+        this.setTitle("Finance Manager");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
 
         //Layout for main frame
-        mainWindow.setLayout(new BorderLayout());
-        mainWindow.setSize(800,800);
-        mainWindow.setVisible(true);
+        this.setLayout(new BorderLayout());
+        this.setSize(800,800);
+        this.setVisible(true);
         
-        //Main application tabbed pane to cycle between tabs
-        JTabbedPane mainTabbedPane = new JTabbedPane();
+        //Main Window menu bar
+        this.configureMenubar();
         
-        
-        
-        
-        
-
-
     }
 
+    private void configureMenubar(){
+        
+        //Configure Menu Bar 
+        menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        //Configure Menus
+        fileMenu = new JMenu("File");
+        helpMenu = new JMenu("Help");
+
+        //Configure Menu Items
+        exportItem = new JMenuItem("Export");
+        exitItem = new JMenuItem("Exit");
 
 
+        fileMenu.add(exportItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+
+        this.setJMenuBar(menuBar);
+    }
+
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 }
--- app/src/main/java/finance/manager/App.java
+++ app/src/main/java/finance/manager/App.java
@@ -3,6 +3,8 @@
  */
 package finance.manager;
 
+import UI.MainWindow;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,5 +12,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+
+        MainWindow window = new MainWindow();
     }
 }
