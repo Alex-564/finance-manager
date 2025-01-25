a374717bffb6245a55c1a699c81f8ee65151cb10
Alex-564 <alex.murray5037@gmail.com>
2025-01-25 18:43:44 +0000

    added basic skeleton for tabbed panel, uploaded images into resources for icon testing, added a project structure guide file

--- app/src/main/java/UI/MainWindow.java
+++ app/src/main/java/UI/MainWindow.java
@@ -9,30 +9,47 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class MainWindow extends JFrame implements ActionListener{
+
+    //Constant variables and PATHs
+    static String RESOURCES_PATH = "../resources/"; 
     
+    //Menu Declarations
     JMenuBar menuBar;
     ArrayList<JMenu> menus = new ArrayList<>();
     ArrayList<JMenuItem> menuItems = new ArrayList<>();
 
+    //Tabbed Page Declarations
+    JTabbedPane tabbedPane;
+    ArrayList<JPanel> tabbedPanels = new ArrayList<>();
+
+    //Constructor
     public MainWindow(){
         //Default settings for main application window
         this.setTitle("Finance Manager");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
+        
 
         //Layout for main frame
         this.setLayout(new BorderLayout());
-        this.setSize(800,800);
-        this.setVisible(true);
-        
+        this.setSize(600,600);
+
         //Main Window menu bar
-        this.configureMenubar();
+        this.configureMenuBar();
+        this.configureTabbedMenus();
         
+
+        //Finally display the window
+        this.setResizable(false);
+        this.setVisible(true);
+         
     }
 
-    private void configureMenubar(){
+    //Set up the menu bar for the main application
+    private void configureMenuBar(){
         //Configure Menu Bar 
         menuBar = new JMenuBar();
         this.setJMenuBar(menuBar);
@@ -50,18 +67,44 @@ public class MainWindow extends JFrame implements ActionListener{
             menuBar.add(menu);
         
             //Each menu has associated menuItems to be added
-            for (String menuItemName : menuItemNames[i]){
-                //Dynamically add the menu items to the menus and menuItems list
-                JMenuItem menuItem = new JMenuItem(menuItemName);
-                menuItems.add(menuItem);
-                menu.add(menuItem);
-
-                //Add action listeners to each menu item
-                menuItem.addActionListener(this);
+            try {
+                for (String menuItemName : menuItemNames[i]){
+                    //Dynamically add the menu items to the menus and menuItems list
+                    JMenuItem menuItem = new JMenuItem(menuItemName);
+                    menuItems.add(menuItem);
+                    menu.add(menuItem);
+    
+                    //Add action listeners to each menu item
+                    menuItem.addActionListener(this);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                break;
             }
+    
         }
 
         //this.setJMenuBar(menuBar);
+        this.revalidate();
+        this.repaint();
+    }
+
+    //Set up the tabbed panel for the Main Application
+    private void configureTabbedMenus(){
+        
+        tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
+
+        JPanel homePanel = new JPanel();
+        JPanel updatePanel = new JPanel();
+
+
+        tabbedPane.addTab("Home", null, homePanel, null);
+        tabbedPane.addTab("Update", null, updatePanel, null);
+
+        this.add(tabbedPane, BorderLayout.CENTER);
+
+
+        this.revalidate();
+        this.repaint();
     }
 
     
